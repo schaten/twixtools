b47c47df5aa0cad9a7c15442fa0de12f4ee77a23
Philip Schaten <philip.schaten@tugraz.at>
2024-05-28 16:05:55 +0200

    util to show dat starting time

---
+++ utils/twix_starttime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import twixtools
+import datetime
+
+parser = argparse.ArgumentParser(description="Output the acquisition start time of the first scan in the datfile in iso format.")
+parser.add_argument("datfile", type=str, help="Path to .dat file")
+args = parser.parse_args()
+
+twix = twixtools.read_twix(args.datfile, parse_data=True, verbose=False, parse_pmu=False)
+
+# timestamp is in 2.5ms since midnight.
+ts = twix[0]['mdb'][0].mdh.TimeStamp
+seconds = ts // 400
+us = (ts - 400 * seconds) * 2500
+
+t = datetime.datetime.fromtimestamp(seconds).time()
+t.replace(microsecond = us)
+
+print(t.isoformat(timespec="milliseconds"))
