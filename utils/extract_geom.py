5db3c368ee9ad31828cac8a24c04f356aa0533c4
Philip <philip.schaten@tugraz.at>
2024-11-03 15:01:05 +0100

    fix fov

--- utils/extract_geom.py
+++ utils/extract_geom.py
@@ -5,29 +5,47 @@ import numpy as np
 from bart import cfl
 import twixtools
 
-radial = True
+def bart_geom(geom, radial):
 
+    fov = [geom.fov[0], geom.fov[0] if radial else geom.fov[1]]
 
-def bart_geom(geom):
+    # geom[0][j] = fov_readout * normalize(readout vector)
+    # geom[1][j] = fov_phase * normalize(phase enc. vector)
+    # geom[2][j] = slice offset
+    # basis: patient coordinates
+    geom = np.array([fov[0] * geom.prs_to_pcs()[:,1],
+                     fov[1] * geom.prs_to_pcs()[:,0],
+                     geom.offset])
+    return geom
 
-    return np.array([geom.fov[1] * geom.prs_to_pcs()[:,1],
-                     geom.fov[0] * geom.prs_to_pcs()[:,0],
-                     geom.offset / np.linalg.norm(geom.offset)]).T
-
-def bart_geom_slices(geom_array, slice_dim = 13):
+def bart_geom_slices(geom_array, slice_dim = 13, slice_order = None, radial = False):
+    if slice_order is None:
+        slice_order = list(range(len(geom_array)))
     return np.reshape(np.stack(
-        [bart_geom(s) for s in geom_array],\
-        axis=-1), tuple([3,3] + (slice_dim - 2) * [1] + [2]))
+        [bart_geom(geom_array[i], radial) for i in slice_order],\
+        axis=-1), tuple([3,3] + (slice_dim - 2) * [1] + [len(geom_array)]))
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("datfile")
     parser.add_argument("outfile")
+    parser.add_argument("--radial", action='store_true')
     args = parser.parse_args()
 
     twix = twixtools.read_twix(args.datfile, parse_geometry=True, parse_data=False)
 
-    geom = bart_geom_slices(twix[-1]['geometry'])
+    order = None
+    if '-' != twix[-1]['hdr']['Config']['chronSliceIndices'][0]:
+        order = []
+        for x in twix[-1]['hdr']['Config']['chronSliceIndices']:
+            if len(order) == len(twix[-1]['geometry']):
+                break
+            if x == ' ':
+                continue
+            val = int(x)
+            order.append(val)
+
+    geom = bart_geom_slices(twix[-1]['geometry'], slice_order = order, radial = args.radial)
 
     cfl.writecfl(args.outfile, geom)
